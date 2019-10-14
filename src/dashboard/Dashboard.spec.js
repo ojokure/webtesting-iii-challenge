21d9695697ab3323e1efbe87ffac8258104337e7
oladimeji-ojo <ojokuredim@gmail.com>
2019-10-14 18:42:55 +0100

    wrote a unit test for dashboard component

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -4,3 +4,21 @@ import React from 'react';
 import * as rtl from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import Dashboard from './Dashboard';
+
+
+afterEach(rtl.cleanup);
+
+let wrapper;
+
+beforeEach(() => {
+    wrapper = rtl.render(<Dashboard />);
+});
+
+describe('Dashboard', () => {
+    test('shows the controls and display', () => {
+        expect(wrapper.queryByText(/unlocked/i)).toBeInTheDocument();
+        expect(wrapper.queryByText(/open/i)).toBeInTheDocument();
+        expect(wrapper.queryByText(/lock gate/i)).toBeInTheDocument();
+        expect(wrapper.queryByText(/close gate/i)).toBeInTheDocument();
+    })
+})
